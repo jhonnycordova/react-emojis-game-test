67e8936d0e1c464daf3564c5c762bb2a5ae05be8
Jhonny Cordova <jhonny.cordova@dsarhoya.cl>
2021-07-12 17:40:01 -0400

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import './App.css';
-import Title from './components/Title';
-import Emojis from './components/Emojis';
-import MovieSearch from './components/MovieSearch';
-import { useState } from 'react'
-
+import "./App.css";
+import Title from "./components/Title";
+import Emojis from "./components/Emojis";
+import MovieSearch from "./components/MovieSearch";
+import { useState } from "react";
 
 function App() {
   let movies = [
-    { name: 'Joker', emojis: '😖🤡🔫' },
-    { name: 'Gremlins', emojis: '🐼🍔🕐👹' },
-    { name: 'Batman', emojis: '🤵🏾🦇' },
+    { name: "Joker", emojis: "😖🤡🔫" },
+    { name: "Gremlins", emojis: "🐼🍔🕐👹" },
+    { name: "Batman", emojis: "🤵🏾🦇" },
   ];
 
-  let index = 0;
+  let [index, setIndex] = useState(0);
   const [movie, setMovie] = useState(movies[index]);
-  const [movieName, setMovieName] = useState('');
+  const [movieName, setMovieName] = useState("");
 
   const handleSearch = () => {
-    if (movieName.toLowerCase() !== movie.name.toLowerCase()) {
+    if (movieName.toLowerCase() !== movie?.name.toLowerCase()) {
       return;
     }
-    setMovieName('');
-    setMovie(movies[index + 1]);
-  }
+    setMovieName("");
+    setIndex(++index);
+    setMovie(movies[index]);
+  };
 
   const handleMovieChanged = (movieName) => {
     setMovieName(movieName);
-  }
+  };
 
-  return (<div className="App">
-    <Title text="Guess the movie" />
-    <Emojis movieEmoji={movie.emojis}></Emojis>
-    <MovieSearch 
-      handleSearch={handleSearch} 
-      handleMovieChanged={handleMovieChanged} 
-      movieName={movieName}>
-    </MovieSearch>
-  </div>);
+  return (
+    <div className="App">
+      <Title text="Guess the movie" />
+      <Emojis movieEmoji={movie?.emojis}></Emojis>
+      <MovieSearch
+        handleSearch={handleSearch}
+        handleMovieChanged={handleMovieChanged}
+        movieName={movieName}
+      ></MovieSearch>
+    </div>
+  );
 }
 
 export default App;
